3395b1b2c04e1df3e2f1c8acfa019f170790fc1f
kevinflynn <flynny85@gmail.com>
2022-09-15 00:37:16 +0100

    wip

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # app-short-cal
 
 Create a fancy calendar pages from iCal data
+
+Visualise block/masonry displays of:
+    * Days in a month
+    * Weeks in a year
+    * Months in a year
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { ref } from "vue";
 // import { useCounterStore } from "@/stores/counter";
-import Month from "@/components/Month.vue";
+// import Month from "@/components/Month.vue";
 
 const september = {
   name: "September",
   id: 8,
   dates: [
-    [1, 2, 3],
+    [2, 3],
     [6, 7],
     [12, 13, 14],
     [16],
@@ -40,28 +40,97 @@ const isSameDate = (date01, date02) => {
 
 let monthData = ref([]);
 
-const monthBuilder = (month, columns) => {
-  const result = month.dates.reduce((acc, item, idx) => {
-    const minDate = getMin(item);
-    const maxDate = getMax(item);
+const createCalObject = () => {
+  return { row: { count: 0 }, groups: [] };
+};
 
-    item.forEach((date) => {
-      acc.push(date);
+const groupBuilder = (group, offset, colCount) => {
+  let build = {
+    spacer: 0,
+    offset: offset,
+    fullLine: false,
+    newLine: false,
+    days: [],
+  };
+
+  const minGroupDate = getMin(group);
+  const maxGroupDate = getMax(group) + 1;
+
+  for (let i = minGroupDate, max = maxGroupDate; i < max; i++) {
+    build.days.push({
+      value: i,
+      count: build.offset % colCount,
     });
+    build.offset += 1;
+  }
 
-    if (isSameDate(minDate, maxDate)) {
-      // acc.push(null);
-      // return acc;
-    }
+  build.newLine =
+    build.days.length > 1 && build.days.some((item) => item.count === 0);
+  build.fullLine = build.days.length > colCount;
+
+  return build;
+};
+
+const monthBuilder = (month, colCount) => {
+  const result = month.dates.reduce((acc, group, idx) => {
+    const minRangeDate = getMin(group);
+    const maxRangeDate = getMax(group) + 1;
+
+    const rangeLength = maxRangeDate - minRangeDate;
+    // const rowCount = acc.row.count + rangeLength;
+
+    let build = groupBuilder(group, acc.row.count, colCount);
+    acc.row.count += rangeLength;
+
+    const spaceLeft = acc.row.count % colCount;
+    // if (spaceLeft > 0) {
+    //   const forceNewLine = build.newLine && build.fullLine;
+    //   build.spacer = forceNewLine ? 1 : spaceLeft;
+    //   acc.row.count += forceNewLine ? 1 : spaceLeft;
+    // }
+    build.spacer = spaceLeft;
+    acc.row.count += spaceLeft;
+
+    acc.groups.push(build);
+    // console.log(acc.row.count, spaceLeft);
+
+    // add spacer
+    // console.log(acc.row.count, colCount - acc.row.count);
+    // acc.row.count += rangeLength;
+    // build.extra = rangeLength < colWidth;
+
+    //check if row needs more push
+
+    // const rowPre = colWidth - (acc.row.count % colWidth);
+    // const rowPost = colWidth - ((acc.row.count + rangeLength) % colWidth);
+    //
+    // console.log(rowPre, rowPost);
+
+    // console.log(JSON.stringify(acc.row));
+
+    // range.forEach((date) => {
+    //   acc.push(date);
+    // });
+    //
+    // if (isSameDate(minRangeDate, maxRangeDate)) {
+    //   // acc.push(null);
+    //   // return acc;
+    // }
 
     // group spacer
-    acc.push(false);
+    // acc.push(false);
 
     return acc;
-  }, []);
+  }, createCalObject());
 
   monthData.value = result;
   return result;
+};
+
+const updateDisplay = (value) => {
+  chosenWidth.value = value;
+
+  monthBuilder(september, value);
 };
 
 // const updateMonth = () => {
@@ -82,26 +151,44 @@ const monthBuilder = (month, columns) => {
 
 <template>
   <div>
-    <button @click="monthBuilder(september)">RUN</button>
-
     <br />
 
-    <template :key="count" v-for="count in [2, 3, 4, 5, 6]">
-      <button @click="updateWidth(count)">{{ count }}</button>
+    <template :key="count" v-for="count in [2, 3, 4, 5, 6, 7]">
+      <button @click="updateDisplay(count)">{{ count }}</button>
     </template>
 
     <!--    <button @click="updateMonth">++</button>-->
 
-    <div
+    <template
       class="container"
       :style="{
         width: `${chosenWidth * 2.5}rem`,
       }"
     >
-      <template :key="date" v-for="date in monthData">
-        <div>{{ date }}</div>
+      <template class="dates">
+        <template :key="group" v-for="group in monthData.groups">
+          <template :key="day" v-for="day in group.days">
+            <div class="day">
+              <p>{{ day.value }}</p>
+              <span>{{ day.count }}</span>
+            </div>
+          </template>
+          <div class="spacer" :key="spacer" v-for="spacer in group.spacer">
+            {{ spacer }}
+          </div>
+        </template>
       </template>
       <!--      <Month :columns="chosenWidth" :month="chosenMonth" />-->
+    </template>
+
+    <div>
+      <template :key="group" v-for="group in monthData.groups">
+        <div>
+          <span>{{ group.spacer }}</span> | <span>{{ group.offset }}</span> |
+          <span>{{ group.newLine }}</span> | <span>{{ group.fullLine }}</span> |
+          <span>{{ group.days.length }}</span>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -124,13 +211,26 @@ const monthBuilder = (month, columns) => {
 
   .spacer,
   .day {
+    position: relative;
     display: inline-block;
     width: 2.5rem;
     height: 2.5rem;
     /*margin: 0.5rem;*/
-    background-color: #565d73;
-    color: #f2f2f2;
+    background-color: #7188d2;
     padding: 0.75rem;
+
+    p {
+      font-size: 1.33rem;
+      color: #f2f2f2;
+    }
+
+    span {
+      font-size: 0.8rem;
+      position: absolute;
+      color: #626262;
+      top: 0;
+      right: 0;
+    }
   }
 
   .spacer {

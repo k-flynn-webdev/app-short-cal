6a32991a4069c733e0e295f3bc53c77d6cb9d6a0
kevinflynn <flynny85@gmail.com>
2023-01-16 11:36:21 +0000

    add login page wip

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+// import * as dotenv from "dotenv";
+// dotenv.config();
 
 import App from "@/App.vue";
 import router from "./router";
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+const gitHubLogin = import.meta.env.VITE_GITHUB_LOGIN;
+</script>
 
 <template>
   <main>
     <h1>Login</h1>
+
+    <a :href="gitHubLogin">Login with Github</a>
   </main>
 </template>

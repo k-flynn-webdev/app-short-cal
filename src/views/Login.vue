b96b1778a075b4019c08ccf3aaa0b9004213b3f9
kevinflynn <flynny85@gmail.com>
2023-01-17 18:29:15 +0000

    add google login button

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,6 +3,7 @@ import { useUserStore } from "@/stores/user";
 const { getUsersAPI } = useUserStore();
 
 const gitHubLogin = import.meta.env.VITE_GITHUB_LOGIN;
+const googleLogin = import.meta.env.VITE_GOOGLE_LOGIN;
 </script>
 
 <template>
@@ -10,6 +11,10 @@ const gitHubLogin = import.meta.env.VITE_GITHUB_LOGIN;
     <h1>Login</h1>
 
     <a :href="gitHubLogin">Login with Github</a>
+    <br />
+    <a :href="googleLogin">Login with Google</a>
+
+    <br />
 
     <button @click="getUsersAPI">test connection</button>
   </main>

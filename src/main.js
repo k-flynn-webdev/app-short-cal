6e875b1354306dad99fe4f71884b89deaecf5402
kevinflynn <flynny85@gmail.com>
2023-01-19 13:01:05 +0000

    add simple component library

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { anu } from "anu-vue";
 
 import App from "@/App.vue";
 import router from "./router";
 
+import "uno.css";
+import "anu-vue/dist/style.css";
+import "@anu-vue/preset-theme-default/dist/style.css";
 import "@/assets/main.css";
 
 const app = createApp(App);
 
+app.use(anu);
 app.use(createPinia());
 app.use(router);
 

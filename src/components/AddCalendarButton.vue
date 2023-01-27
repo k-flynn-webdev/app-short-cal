dd77671ff3f4b5ce80f490c52bf2737eee6fad9c
kevinflynn <flynny85@gmail.com>
2023-01-27 13:48:35 +0000

    wup add cal button component

--- src/components/AddCalendarButton.vue
+++ src/components/AddCalendarButton.vue
@@ -14,9 +14,20 @@ const { isLoading, clearLoading, setLoading } = isLoadingFactory();
 const { isError, hasError, clearError, setError } = isErrorFactory();
 
 const calendarTypes = [
-  { name: "Google", value: "google", icon: "fa-brands fa-google" },
-  { name: "Microsoft", value: "microsoft", icon: "fa-brands fa-google" },
-  { name: "Yahoo", value: "yahoo", icon: "fa-brands fa-google" },
+  {
+    name: "Google",
+    value: "google",
+    icon: "fa-brands fa-google",
+    active: true,
+  },
+  { name: "Apple", value: "apple", icon: "fa-brands fa-apple", active: false },
+  {
+    name: "Microsoft",
+    value: "microsoft",
+    icon: "fa-brands fa-microsoft",
+    active: false,
+  },
+  { name: "Yahoo", value: "yahoo", icon: "fa-brands fa-yahoo", active: false },
 ];
 
 const onAddCalendar = async (type) => {
@@ -42,15 +53,21 @@ const onAddCalendar = async (type) => {
             class="mt-4"
             type="button"
             color="primary"
+            :disabled="!item.active"
             @click="onAddCalendar(item.value)"
           >
             <font-awesome-icon :icon="item.icon" />
           </x-button>
 
           <template #tooltip>
-            <span class="text-gray-300">{{
-              `Add a ${item.name} calendar`
-            }}</span>
+            <div class="text-center">
+              <p v-if="!item.active" class="text-yellow-300">
+                {{ "Available soon" }}
+              </p>
+              <p class="text-gray-300">
+                {{ `Add a ${item.name} calendar` }}
+              </p>
+            </div>
           </template>
         </x-tooltip>
       </template>

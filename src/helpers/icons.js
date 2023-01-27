f770f69435719c2409be6a6bce4729611c56131c
kevinflynn <flynny85@gmail.com>
2023-01-27 13:48:19 +0000

    add new icons

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,11 +1,18 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faGoogle,
+  faGithub,
+  faApple,
+  faFacebook,
+  faYahoo,
+  faMicrosoft,
+} from "@fortawesome/free-brands-svg-icons";
 // import { fas } from "@fortawesome/free-solid-svg-icons";
 // import { far } from "@fortawesome/free-regular-svg-icons";
 import { dom } from "@fortawesome/fontawesome-svg-core";
 
-library.add([faGoogle, faGithub]);
+library.add([faGoogle, faGithub, faApple, faFacebook, faYahoo, faMicrosoft]);
 // library.add(fas);
 // library.add(far);
 dom.watch();
